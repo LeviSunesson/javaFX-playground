2c0f1dde89871f95115fe86615e278768ddb7f09
levisunesson <levi@sunesson.com>
2018-05-17 18:02:47 +0200

    Started asteroids

---
+++ src/spaceShooter/Asteroid.java
@@ -0,0 +1,17 @@
+package spaceShooter;
+
+import javafx.scene.Group;
+import javafx.scene.shape.Polygon;
+
+public class Asteroid extends Group{
+
+	Asteroid(){
+		
+		Polygon main = new Polygon();
+		
+		main.setTranslateX(100);
+		main.setTranslateY(100);
+		
+	}
+	
+}
